d8a6245d4634f145c2ec34b7fa20d4d53d133aad
Gitibit <gitibitcraftfeb2022@gmail.com>
2022-04-08 19:41:39 -0700

    hi

---
+++ ExerciseJava/src/com/dayOneExe/MultiplicationTable.java
@@ -0,0 +1,18 @@
+package com.dayOneExe;
+
+public class MultiplicationTable {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for (int a =1; a<=6; a++) {
+			for(int b =1 ; b<=6; b++) {
+				System.out.println("******");
+				System.out.println(a+ "x"+ b+ "="+ a*b);
+				
+			}
+			
+			
+		}
+	}
+
+}
