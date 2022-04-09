93672c0c8b3009ed04b79fa94a44e8713fc7e872
Gitibit <gitibitcraftfeb2022@gmail.com>
2022-04-08 18:46:13 -0700

    new commit

---
+++ ExerciseProject/src/com/exe/java/SwitchDemo.java
@@ -0,0 +1,16 @@
+package com.exe.java;
+
+public class SwitchDemo {
+
+	public static void main(String[] args) {
+		System.out.println("MULTIPLICATION TABLE");
+		for (int i =1; i <= 6; i++) {
+			for(int j = 1;j<=6; j++) {
+				System.out.println(i+"x"+j+ "=" +i*j);		
+				
+			}
+			System.out.println("******");
+		}
+	}
+
+}
