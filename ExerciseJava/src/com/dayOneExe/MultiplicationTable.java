5159913dbffc937f7cc08e17b9b8a0c002bd7935
Gitibit <gitibitcraftfeb2022@gmail.com>
2022-04-11 23:13:13 -0700

    some changes

--- ExerciseJava/src/com/dayOneExe/MultiplicationTable.java
+++ ExerciseJava/src/com/dayOneExe/MultiplicationTable.java
@@ -3,12 +3,17 @@ package com.dayOneExe;
 public class MultiplicationTable {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		for (int a =1; a<=6; a++) {
-			for(int b =1 ; b<=6; b++) {
+		
+		for (int a =1; a<=8; a++) {
+			for(int b =1 ; b<=8; b++) {
 				System.out.println("******");
-				System.out.println(a+ "x"+ b+ "="+ a*b);
-				
+				System.out.println(a+ "x"+ b+ " = "+ a*b);
+				/*the out put looks like this
+					1x1 = 1
+					1x2 = 2
+					1x3 = 3
+					1x4 = 4
+						  */
 			}
 			
 			
@@ -17,9 +22,4 @@ public class MultiplicationTable {
 
 }
 
-/*the out put looks like this
-	1x1 = 1
-	1x2 = 2
-	1x3 = 3
-	1x4 = 4
-		  */
+
